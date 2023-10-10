6bf12906d56113a16207ceb45c80c1239b856028
Krzysztof Wysocki <kotinerus@gmail.com>
2023-10-10 13:13:40 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,68 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import MyImage from "./equation.png";
+import "./scss/index.css";
+export default function App() {
+  const [resistorIsOpen, setResistorIsOpen] = useState(false);
 
-function App() {
+  function handleClick() {
+    setResistorIsOpen(!resistorIsOpen);
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Resistor handleClick={handleClick} />
+      {resistorIsOpen && <ResistorPlatform />}
+    </>
   );
 }
 
-export default App;
+function Resistor({ handleClick }) {
+  return (
+    <>
+      <div className="resistorDiv" onClick={handleClick}>
+        <img src="https://cdn-icons-png.flaticon.com/512/9054/9054099.png" />
+      </div>
+    </>
+  );
+}
+function ResistorPlatform() {
+  return (
+    <div className="resistorPlatform">
+      <img src={MyImage} />
+      <label className="nameLabel" for="nameInput">
+        Nazwa
+      </label>
+      <input className="nameInput" type="text"></input>
+      <label className="voltageLabel" for="voltageInput">
+        Napięcie(V)
+      </label>
+      <input className="voltageInput" type="text"></input>
+      <label className="intensityLabel" for="intensityInput">
+        Natężenie(A)
+      </label>
+      <input className="intensityInput" type="text"></input>
+      <table className="table">
+        <tr>
+          <th>Nazwa</th>
+          <th>Napięcie(U)</th>
+          <th>Natężenie(I)</th>
+          <th>Delta napięcia (dU)</th>
+          <th>Delta natężenia (dI)</th>
+        </tr>
+        <tr>
+          <td>A1</td>
+          <td>0.9952</td>
+          <td>0.1026</td>
+          <td>0.0005</td>
+          <td>0.082</td>
+        </tr>
+        <tr>
+          <td>A1</td>
+          <td>0.9952</td>
+          <td>0.1026</td>
+          <td>0.0005</td>
+          <td>0.082</td>
+        </tr>
+      </table>
+    </div>
+  );
+}
